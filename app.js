40907084bfe59d8fd2d686d187990996a399b55d
king610160 <king610160@gmail.com>
2023-05-01 14:39:37 +0800

    feat: set package.json's scripts. add mongoose schema in modules files

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ if (process.env.MONGODB_URI !== 'production') {
   }
 
 const express = require('express')
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars')
 const PORT = process.env.PORT
 require('./config/mongoose')
 
 const app =  express()
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
-app.get('/',(req,res)=>{
+
+app.get('/',(req, res) => {
     res.render('index')
 })
 
-app.listen(PORT,()=>{
+
+app.listen(PORT, () => {
     console.log(`App is listening on loaclhost:${PORT}`)
 })
