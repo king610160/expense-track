d3fb616f405009c292974a2e7f7888be1b45cdae
king610160 <king610160@gmail.com>
2023-05-06 23:50:39 +0800

    feat: add users.js for login and register.hbs

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
+const users = require('./modules/users')
 const expenseTrack = require('./modules/expenseTrack')
 
 router.use('/', home)
+router.use('/users', users)
 router.use('/expenseTrack', expenseTrack)
 
 module.exports = router
