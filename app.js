582a579521155a4bd3b06b6991d53f40c5469238
king610160 <king610160@gmail.com>
2023-05-07 10:59:22 +0800

    feat:add auth.js for facebook login, modified passport for facebook login.

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
-  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
-  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  res.locals.success_msg = req.flash('success_msg') 
+  res.locals.warning_msg = req.flash('warning_msg') 
   next()
 })
 
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const home = require('./modules/home')
 const users = require('./modules/users')
 const expenseTrack = require('./modules/expenseTrack')
 const { authenticator } = require('../middleware/auth')
+const auth = require('./modules/auth')
 
 
 router.use('/users', users)
 router.use('/expenseTrack', expenseTrack)
+router.use('/auth', auth)
 router.use('/', authenticator,home)
 
 module.exports = router
