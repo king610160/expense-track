0fdc542db8c88f2465bf96040e1a917137cc572e
king610160 <king610160@gmail.com>
2023-05-07 14:47:22 +0800

    feat: add recordSeeder, change edit, index, new.hbs design.

--- models/seed/recordSeeder.js
+++ models/seed/recordSeeder.js
@@ -1,20 +1,45 @@
-if (process.env.MONGODB_URI !== 'production') {
-    require('dotenv').config()
-  }
+const bcrypt = require('bcryptjs')
+if (process.env.PROCESS_ENV !== 'production') {
+  require('dotenv').config()
+}
 
-const mongoose = require('mongoose')
-const expenseTrack = require('../expenseTrack') // 載入 todo model
+const db = require('../../config/mongoose')
+const ExpenseTrack = require("../expenseTrack")
+const ExpenseTrackList = require("./expenseTrack.json").results
+const Member = require('../users')
+const memberData = require('./users.json')
 
+db.once("open", () => {
+  console.log("running recordSeeder script...")
+  const promises = memberData.map(USER => {
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(USER.password, salt))
+      .then(hash => Member.create({
+        name: USER.name,
+        email: USER.email,
+        password: hash
+      }))
+      .then(user => {
+        const userId = user._id
+        const name = user.name
+        let expenseTrack = []
+        if (name === memberData[0].name) {
+          expenseTrack = ExpenseTrackList.slice(0, 5)
+        } else {
+          expenseTrack = ExpenseTrackList.slice(5, 10)
+        }
+        return ExpenseTrack.create(
+          expenseTrack.map(r => Object.assign(r, { userId }))
+        )
+      })
+      .catch(err => console.log(err))
+  })
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-  for (let i = 0; i < 10; i++) {
-    expenseTrack.create({event:`event-${i}`})
-  }
-  console.log('done')
+  Promise.all(promises)
+    .then(() => {
+      console.log('recordSeeder done!')
+      process.exit()
+    })
+    .catch(err => console.log(err))
 })
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,18 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const ExpenseTrack = require('../../models/expenseTrack')
-// const expenseTrack = require('../../models/expenseTrack')
+const CategorySeed = require('../../models/seed/categorySeeder').results
 
-router.get("/", (req, res, next) => {
+
+
+router.get("/", (req, res) => {
     const userId = req.user._id   // 變數設定
     ExpenseTrack.find({ userId })   // 加入查詢條件
     .lean()
     .sort({ _id: 'asc' }) 
     .then((expenseTrack) => {
         let sum = 0
+        let a = ''
         for (i in expenseTrack) {
             expenseTrack[i].date = expenseTrack[i].date.toLocaleDateString()
             sum += expenseTrack[i].money
+            if (expenseTrack[i].category === '餐飲食品') {
+                a = CategorySeed.slice(0, 1).value
+                console.log(a)
+            }
         }
         res.render("index", { expenseTrack ,sum })
     })
